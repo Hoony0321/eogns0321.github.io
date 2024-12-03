593003bbeb07899422f18be192e70e921ef06672
Hoony0321 <eogns0321@gmail.com>
2024-12-02 23:39:22 -0800

    refactor : change blog-config info to mine

--- blog-config.js
+++ blog-config.js
@@ -1,28 +1,28 @@
 module.exports = {
-  title: "hoodie",
-  description: "Hello :) I'm Hudi who developed gatsby-starter-hoodie theme.",
-  author: "Hudi",
-  siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
+  title: "DAEHUN GO",
+  description: "Hello, I'm Daehun Go, a passionate developer.",
+  author: "DAEHUN GO",
+  siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/", // TODO : CHANGE
   links: {
-    github: "https://github.com/devHudi",
-    linkedIn: "https://linkedin.com",
-    facebook: "https://www.facebook.com",
-    twitter: "https://twitter.com",
-    x: "https://x.com",
-    instagram: "https://www.instagram.com",
-    medium: "https://medium.com",
-    blogger: "https://blogger.com",
-    email: "mailto:devhudi@gmail.com",
-    resume: "https://hudi.blog",
-    link: "https://hudi.blog",
+    github: "https://github.com/hoony0321",
+    linkedIn: "https://www.linkedin.com/in/daehungo/",
+    facebook: "https://www.facebook.com", // TODO : CHANGE
+    twitter: "https://twitter.com", // TODO : CHANGE
+    x: "https://x.com", // TODO : CHANGE
+    instagram: "https://www.instagram.com", // TODO : CHANGE
+    medium: "https://medium.com", // TODO : CHANGE
+    blogger: "https://blogger.com", // TODO : CHANGE
+    email: "eogns0321@gmail.com",
+    resume: "https://hudi.blog", // TODO : CHANGE
+    link: "https://hudi.blog", // TODO : CHANGE
   },
   useAbout: true,
   // See https://giscus.app/
   giscus: {
-    repo: "devHudi/gatsby-starter-hoodie",
-    repoId: "MDEwOlJlcG9zaXRvcnkzNjk4NjMzNTg=",
+    repo: "hoony0321/hoony0321.github.io",
+    repoId: "897069565",
     category: "Comments",
-    categoryId: "DIC_kwDOFguqvs4ChwGy",
+    categoryId: "DIC_kwDONXgx_c4Ck17u",
     mapping: "pathname",
     strict: "0",
     reactionsEnabled: "1",
